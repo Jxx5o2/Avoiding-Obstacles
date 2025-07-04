cf19b6d30c067d1e2ca5c3bea3fb80058c7776a4
Jxx5o2 <vtsun98@gmail.com>
2025-07-04 19:32:04 +0900

    태그 이용하기
    장애물과 플레이어에 태그를 붙여서, 특정 태그를 만족할 때만 코드가 실행 되도록 함

--- Avoiding Obstacles/Assets/Scripts/objectHit.cs
+++ Avoiding Obstacles/Assets/Scripts/objectHit.cs
@@ -2,8 +2,13 @@ using UnityEngine;
 
 public class objectHit : MonoBehaviour
 {
-    private void OnCollisionEnter(Collision collision)
+    private void OnCollisionEnter(Collision other)
     {
-        GetComponent<MeshRenderer>().material.color = Color.black;        
+        if (other.gameObject.tag == "Player")
+        {
+            GetComponent<MeshRenderer>().material.color = Color.black;
+            gameObject.tag = "Hit";
+        }
+                
     }
 }
